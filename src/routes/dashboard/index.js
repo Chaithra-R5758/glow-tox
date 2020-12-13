02bc5bedefa5857b31e94c4e9de46ea00578ecf8
akash hamse <akashhamse@akashs-MacBook-Air.local>
2020-12-13 09:42:37 +0530

    dashboard screen

--- src/routes/dashboard/index.js
+++ src/routes/dashboard/index.js
@@ -1,16 +1,15 @@
-import './dashboard.scss';
 import Header from '../../Header'
 import Footer from '../../components/footer/'
 import Navbar from '../../components/navbar/'
 import { PageTitle } from '../../components/page-title/'
 import { DASHBOARD_OPTIONS } from '../../constants/';
-
+import './dashboard.scss';
 import { Card } from 'antd';
 const { Meta } = Card;
 
 function Dashboard() {
   return (
-    <div className="App">
+    <div className="dashboard-screen">
       <Header />
       <div>
         <Navbar />
@@ -25,7 +24,7 @@ function Dashboard() {
                   <Card
                     hoverable
                     style={{ width: 240 }}>
-                    <Meta title="Europe Street beat"
+                    <Meta title="Europe Street beat12"
                       description="www.instagram.com"
                     />
                   </Card>
