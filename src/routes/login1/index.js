efda54ef6f140d4ed3823d1d589a529a545265de
chaithra <r.chaithra19@gmail>
2021-01-08 13:10:03 +0530

    first commit

--- src/routes/login1/index.js
+++ src/routes/login1/index.js
@@ -1,6 +1,6 @@
 import { withRouter, } from "react-router-dom";
 import React, { Component, Suspense } from 'react';
-import { Form, Input, Button, Checkbox } from 'antd';
+import { Form, Input, Button, Checkbox ,Card,Image} from 'antd';
 import brandLogo from '../../assets/brand-logo.png'
 import './login.scss'
 import Cookies from 'js-cookie';
@@ -9,7 +9,7 @@ class Login extends Component {
 
     signInClicked = () => {
         Cookies.set('accessToken', 'value');
-        this.props.history.push('/dashboard')
+        this.props.history.push('/login1')
         window.location.reload();
     }
 
@@ -35,52 +35,62 @@ class Login extends Component {
         const onFinishFailed = (errorInfo) => {
             console.log('Failed:', errorInfo);
         };
+        
 
         return (
             <div className={'login-screen'}>
+                <Card style={{width:'400px' ,marginTop:'111px',marginLeft:'290px'}}>
+                 <Image 
+      width={350} src="https://image.freepik.com/free-vector/vector-illustration-home-office-computer-stationery-items-houseplants-desk_173706-73.jpg"/>
+                </Card>
                 <div className={'login-wrapper'}>
+                    
                     <Form
-                        {...layout}
-                        name="basic"
-                        initialValues={{
-                            remember: false,
-                        }}
-                        onFinish={onFinish}
-                        onFinishFailed={onFinishFailed}>
+                    layout="vertical"
+                    name="normal_login"
+                    className="login-form"
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish} >
+                          
                         <div className={'login-brand-logo-wrapper'}>
-                            <img src={brandLogo} className={'login-brand'} />
+                            <div className={'login-brand'}>
+                               <h1>GLOWTOX</h1>
+                               </div> 
                         </div>
-                        <Form.Item
-                            label="Username"
+                        <Form.Item 
+                            label="Login"
                             name="username"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Please input your username',
-                                },
-                            ]}
-                        >
-                            <Input />
+        rules={[{ required: true, message: 'Please input your Username!' }]}
+        >
+                            <Input  placeholder="Username" />
                         </Form.Item>
                         <Form.Item
-                            label="Password"
-                            name="password"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Please input your password',
-                                },
-                            ]}
-                        >
-                            <Input.Password />
+                             name="password"
+                             rules={[{ required: true, message: 'Please input your Password!' }]}
+                           >
+                             <Input
+                               prefix={<LockOutlined className="site-form-item-icon" />}
+                               type="password"
+                               placeholder="Password"
+                             />
                         </Form.Item>
 
-                        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-                            <Checkbox>Remember me</Checkbox>
-                        </Form.Item>
-                        <div className={'login-btn'} onClick={() => this.signInClicked()}>
-                                Submit
-                            </div>
+                        <Form.Item>
+        <Form.Item name="remember" valuePropName="checked" noStyle>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <a className="login-form-forgot" href="">
+          Forgot password
+        </a>
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Log in
+        </Button>
+        Or <a href="">register now!</a>
+      </Form.Item>
                     </Form>
                 </div>
             </div>
