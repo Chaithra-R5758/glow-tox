87ea4f1228c05d61d557b47379e9fd9f05640eb8
chaithra <r.chaithra19@gmail>
2021-01-18 13:19:34 +0530

    changes

---
+++ src/routes/promotions/dist/mock.dev.js
@@ -0,0 +1,38 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.response = void 0;
+var response = {
+  "status": 200,
+  "promotion": [{
+    "recId": "recfU4j437EPXnR6B",
+    "promoId": "P00004",
+    "isActive": true,
+    "promoName": "New Year Offerss",
+    "description": "New Year Offer",
+    "promoImage": "https://pbs.twimg.com/profile_images/1227540838596562944/bs5ZQkrs.jpg",
+    "service": "rec2qBN2cMoUIYCmJ",
+    "offer": 30,
+    "promoCode": "NEW30",
+    "id": 4
+  }, {
+    "recId": "recMn7mx4ASFyZyfb",
+    "promoId": "P00009",
+    "isActive": true,
+    "promoName": "Ugadi",
+    "description": "Ugadi",
+    "promoImage": "https://pbs.twimg.com/profile_images/1227540838596562944/bs5ZQkrs.jpg",
+    "service": "rec2qBN2cMoUIYCmJ",
+    "offer": 40,
+    "promoCode": "NEW40",
+    "id": 9
+  }, {
+    "recId": "reclEjiiiThMPiM1O",
+    "promoId": "P00010",
+    "service": null,
+    "id": 10
+  }]
+};
+exports.response = response;
