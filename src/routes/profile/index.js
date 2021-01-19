bd5bed437c11fc4e8140f92b6c8218fc325a7425
chaithra <r.chaithra19@gmail>
2021-01-19 17:01:57 +0530

    profile pic upload

--- src/routes/profile/index.js
+++ src/routes/profile/index.js
@@ -4,101 +4,129 @@ import './profile.scss';
 import { Card, Input, Space, Image, Button, Anchor } from 'antd';
 import { EditFilled } from '@ant-design/icons'
 import { response } from './mock.js'
-const { Link } = Anchor;
-//const { Meta } = Card;
+import { withRouter } from 'react-router-dom';
+import React, { Component } from 'react';
+
 const layout = {
     labelCol: { span: 10 },
     wrapperCol: { span: 16 },
 };
 const { Option } = Select;
 
-function Profile() {
-    return (
-        <div className="profile-screen">
-            <div>
-                <div className={'content-wrapper'}>
-                    <PageTitle
-                        title={'Profile'}
-                    />
-                    <div className={'content-body-wrapper'}>
-                        <div className={'profile-card'} >
-                            <Card >
-                                <div className={'title-card'}>
-                                    Personal Profile 
+class Profile extends React.Component {
+    state = {
+        profileImg:
+            "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+    };
+
+    imageHandler = e => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            if (reader.readyState === 2) {
+                this.setState({ profileImg: reader.result });
+            }
+        };
+        reader.readAsDataURL(e.target.files[0]);
+    };
+    render() {
+        const { profileImg } = this.state;
+        return (
+            <div className="profile-screen">
+                <div>
+                    <div className={'content-wrapper'}>
+                        <PageTitle
+                            title={'Profile'}
+                        />
+                        <div className={'content-body-wrapper'}>
+                            <div className={'profile-card'} >
+                                <Card >
+                                    <div className={'title-card'}>
+                                        Personal Profile
                            </div>
-                                <div className={'img-card'}>
-                                    <img src={response.user.profilePic ? response.user.profilePic : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTElqQujDQxdUSZiRtY81HGprrV_jEvxBZ14Q&usqp=CAU'}
-                                        className={'profile-img'} />
-                                </div>
-                                <div className="edit-btn-card">
-                                    {<EditFilled style={{ color: "#343557", fontSize: '1.5em' }} />}
-                                </div>
-                                <Space direction="vertical">
-                                    <div className="profile-card-body" >
-                                        <Form
-                                            layout="vertical"
-                                            name="nest-profile" >
-                                            <Form.Item name={['user', 'name']} label="Full Name" >
-                                                <Input 
-                                                    value={response.user.userName}
-                                                    size="large" 
-                                                    style={{ borderRadius: '5px' }} 
-                                                />
-                                            </Form.Item>
-                                            <Form.Item name={['user', 'email']} label="Email Id"
-                                                rules={[{ type: 'email' }]}>
-                                                <Input 
-                                                    value={response.user.emailId}
-                                                    size="large" 
-                                                    style={{ borderRadius: '5px' }} />
-                                            </Form.Item>
-                                            <Form.Item name={['user', 'phone']} label="Phone Number"
-                                                rules={[{ type: 'phoneNum' }]}>
-                                                <Input 
-                                                    size="large" 
-                                                    style={{ borderRadius: '5px' }} 
-                                                    value={response.user.phoneNumber}
-                                                />
-                                            </Form.Item>
-                                        </Form>
+                                    <div className={'img-card'}>
+                                        <img alt="" id="img" src={profileImg}
+                                            className={'profile-img'} />
                                     </div>
-                                    <div className={'profile-primary-btn'} >
-                                        Submit
+                                    <div className="edit-btn-card">
+                                        <label className htmlFor="input">
+                                            <i type="link" style={{ color: "#343557", fontSize: '1.5em' }}>
+                                                {<EditFilled />} </i>
+                                        </label>
                                     </div>
-                                </Space>
-                            </Card>
-                        </div>
-                        <div className={'profile-card-pwd'}>
-                            <Card>
-                                <div className={'title-card'}>
-                                    Change Password
+                                    <input style={{ display: 'none' }}
+                                        type="file"
+                                        accept="image/*"
+                                        name="image-upload"
+                                        id="input"
+                                        onChange={this.imageHandler}
+                                    />
+
+                                    <Space direction="vertical">
+                                        <div className="profile-card-body" >
+                                            <Form
+                                                layout="vertical"
+                                                name="nest-profile" >
+                                                <Form.Item name={['user', 'name']} label="Full Name" >
+                                                    <Input
+                                                        value={response.user.userName}
+                                                        size="large"
+                                                        style={{ borderRadius: '5px' }}
+                                                    />
+                                                </Form.Item>
+                                                <Form.Item name={['user', 'email']} label="Email Id"
+                                                    rules={[{ type: 'email' }]}>
+                                                    <Input
+                                                        value={response.user.emailId}
+                                                        size="large"
+                                                        style={{ borderRadius: '5px' }} />
+                                                </Form.Item>
+                                                <Form.Item name={['user', 'phone']} label="Phone Number"
+                                                    rules={[{ type: 'phoneNum' }]}>
+                                                    <Input
+                                                        size="large"
+                                                        style={{ borderRadius: '5px' }}
+                                                        value={response.user.phoneNumber}
+                                                    />
+                                                </Form.Item>
+                                            </Form>
+                                        </div>
+                                        <div className={'profile-primary-btn'} >
+                                            Submit
+                                    </div>
+                                    </Space>
+                                </Card>
+                            </div>
+                            <div className={'profile-card-pwd'}>
+                                <Card>
+                                    <div className={'title-card'}>
+                                        Change Password
                            </div>
-                                <Space direction="vertical" >
-                                    <div className={'pwd-card-body'}  >
-                                        <Form
-                                            layout="vertical"
-                                            name="nest-profile"  >
-                                            <Form.Item name={['user', 'password']} label="Password"
-                                                rules={[{ type: 'password' }]}  >
-                                                <Input.Password size="large" style={{ borderRadius: '5px' }} />
-                                            </Form.Item>
-                                            <Form.Item name={['user', 'Confirm']} label="Re-type Password"
-                                                rules={[{ type: 'password' }]}>
-                                                <Input.Password size="large" style={{ borderRadius: '5px' }} />
-                                            </Form.Item>
-                                        </Form>
+                                    <Space direction="vertical" >
+                                        <div className={'pwd-card-body'}  >
+                                            <Form
+                                                layout="vertical"
+                                                name="nest-profile"  >
+                                                <Form.Item name={['user', 'password']} label="Password"
+                                                    rules={[{ type: 'password' }]}  >
+                                                    <Input.Password size="large" style={{ borderRadius: '5px' }} />
+                                                </Form.Item>
+                                                <Form.Item name={['user', 'Confirm']} label="Re-type Password"
+                                                    rules={[{ type: 'password' }]}>
+                                                    <Input.Password size="large" style={{ borderRadius: '5px' }} />
+                                                </Form.Item>
+                                            </Form>
+                                        </div>
+                                        <div className={'profile-primary-pwd-btn'} >
+                                            Submit
                                     </div>
-                                    <div className={'profile-primary-pwd-btn'} >
-                                        Submit
-                                    </div>
-                                </Space>
-                            </Card>
+                                    </Space>
+                                </Card>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
-
-export default Profile;
+export default withRouter(Profile);
--- src/routes/promotions/index.js
+++ src/routes/promotions/index.js
@@ -3,40 +3,46 @@ import './promotions.scss';
 import { Input, Card, Image,Skeleton } from 'antd';
 import loginImg from '../../assets/login-img.png'
 import { LinkOutlined, EditFilled,CloseOutlined  } from '@ant-design/icons';
-import React, { useState } from 'react';
 import { Modal, Button, Upload } from 'antd';
-import {BrowserRouter as Router,useHistory} from 'react-router-dom'
 import { response } from './mock.js'
 import { responseId } from './mock-id.js'
-const { TextArea } = Input;
+import { withRouter } from 'react-router-dom';
+import React, { Component } from 'react';
 
 
-function Promotions() {
-  const props = {
-    name: 'file',
-    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-    headers: {
-      authorization: 'authorization-text',
-    }
-  }
-
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const showModal = () => {
-    setIsModalVisible(true);
+class Promotions extends React.Component {
+  state = {
+    loginImg
+     
   };
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
-  const history = useHistory();
-  const handleHistory = () => {
-    history.push("/promotions" )
-  }
+  // state = { visible: true };
 
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  hideModal = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+  imageHandler = e => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        this.setState({ loginImg: reader.result });
+      }
+    };
+    reader.readAsDataURL(e.target.files[0]);
+  };
+ render(){
+  const { loginImg } = this.state;
  
   return (
     
-    <Router>
+  
     <div className="promotions-screen">
       <div>
         <div className={'content-wrapper'}>
@@ -47,7 +53,7 @@ function Promotions() {
           <div className={"promotions-card"}>
           <Card>
           <div className={'content-body-wrapper'}>
-            <div className={'primary-btn '} onClick={showModal}>
+            <div className={'primary-btn '} onClick={this.showModal}>
             Add Promo
             </div>
               
@@ -71,30 +77,38 @@ function Promotions() {
                   <div className={'promo-card'}>
                     <Card bordered={true}>
                       <div className="edit-btn-card">
-                        <Button type="link" block style={{ textAlign: "right", color: "#343557" ,fontSize:'1.5em'}} onClick={showModal}>
+                        <Button type="link" block style={{ textAlign: "right", color: "#343557" ,fontSize:'1.5em'}} onClick={this.showModal}>
                           {<EditFilled/>}
                         </Button>
-                        <Modal   visible={isModalVisible} footer={null} onCancel={handleCancel} width={400 } style={{ top: 80}} >
+                        <Modal   
+          visible={this.state.visible}
+          onCancel={this.hideModal} footer={null} width={400 } style={{ top: 80}} >
                 <div className={'add-promo-card'}>
                     <div className="modal-title" style={{fontFamily:"Poppins, sans-serif",
         fontWeight:' bolder', fontSize:'18px',marginTop:20}}>Promotions Edit</div>
         
-        <Button className="save-btn" style={{float:'right', backgroundColor: '#5D72E9',color:'white',borderRadius:'5px',padding:' 0px 25px',marginTop:'-30px'}} onClick={handleHistory}>Save</Button>
+        <Button className="save-btn" style={{float:'right', backgroundColor: '#5D72E9',color:'white',borderRadius:'5px',padding:' 0px 25px',marginTop:'-30px'}}onClick={() => this.props.history.push('/promotions')}>Save</Button>
 
                     
                     
                     <div className={'modal-img-card'} style={{  backgroundColor: ' #E2E2E2',height:150,marginTop:20 ,marginBottom:10}}>
-                   <Upload {...props} ><Button type="link"  style={{marginLeft:300, color: "#343557" ,fontSize:'1.5em'}}>
+                    <label  htmlFor="input"><i type="link"  style={{marginLeft:300, color: "#343557" ,fontSize:'1.5em'}}>
                     {<EditFilled/>}
-                    </Button>
-                    </Upload> 
-                    <img  style={{width:160,transform:'translateX(60%) translateY(-30%) '}}
-                        src={loginImg} />
+                    </i>
+                    </label>
+                    <img  src={loginImg} alt="" id="img" className="img" style={{width: 160, height:130,objectFit:'cover', transform:'translateX(60%) translateY(-20%)'}}/>
+                        </div>
                         
-                    
+                        <input style={{display:'none'}}
+            type="file"
+            accept="image/*"
+            name="image-upload"
+            id="input"
+            onChange={this.imageHandler}
+          />
                     
                      
-                    </div>
+                    
                     <div className="modal-code" style={{fontFamily:"Poppins, sans-serif",
         fontWeight:' bolder', fontSize:'16px',marginBottom:'5px'}}> 
                     Description</div>
@@ -131,7 +145,7 @@ function Promotions() {
                       <div className={'desc-card'}>
                         {promotion.description ? promotion.description : 'No Description'}
                       </div>
-                        <Button className="btn-card" onClick={showModal} icon={<LinkOutlined />} >Link to Services </Button>
+                        <Button className="btn-card" onClick={this.showModal} icon={<LinkOutlined />} >Link to Services </Button>
 
                     </Card>
                   </div>
@@ -145,8 +159,8 @@ function Promotions() {
         </div>
       </div>
     </div>
-    </Router>
+    
   );
 }
-
-export default Promotions;
+}
+export default withRouter(Promotions);
