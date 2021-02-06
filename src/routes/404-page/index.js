0803fc6077eed54960e03de9a9c4bd74dca707c6
akash hamse <akashhamse@akashs-MacBook-Air.local>
2021-02-06 12:04:22 +0530

    functional component

--- src/routes/404-page/index.js
+++ src/routes/404-page/index.js
@@ -1,18 +1,14 @@
 import { Result, Button } from 'antd';
-import React from 'react'
-class  PageNotFound extends React.Component {
-render()
-{
-return(
 
-  <Result
-    status="404"
-    title="404"
-    style={{top:'10%',left:'40%',position:'absolute'}}
-    subTitle="Sorry, the page you visited does not exist."
-    extra={<Button type="primary"><a href="/login">Back Home</a></Button>}
-  />
-);
-}
+const PageNotFound = () => {
+    return (
+      <Result
+        status="404"
+        title="404"
+        style={{ top: '10%', left: '40%', position: 'absolute' }}
+        subTitle="Sorry, the page you visited does not exist."
+        extra={<Button type="primary"><a href="/login">Back Home</a></Button>}
+      />
+    );
 }
 export default PageNotFound;
