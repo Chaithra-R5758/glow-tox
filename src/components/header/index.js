ca4048f7351ec4eb21cc67205b09a534e86f5227
akash hamse <akashhamse@akashs-MacBook-Air.local>
2021-02-06 20:16:41 +0530

    header fix

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,29 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import './header.scss';
+import axios from '../../config/api/'
 
 const Header = () => {
   const history = useHistory();
-    return (
-      <div className={'header'}>
-        <div className={'header-wrapper'}>
-          <span className={'header-title'}>Glow Tox</span>
-          <div className={'profile-name'}>
-            <div className={'profile-inner-wrapper'} onClick={() => history.push({ pathname:  "/profile" })}>
-              <div className={'profile-icon'}>
-                {
-                  <img
+  const [userDetails, setUserDetails] = useState({})
+
+  useEffect(() => {
+    async function getUserDetails() {
+      try {
+        const { data } = await axios.get('admin')
+        const userDetails = (data && data.user) || ''
+        if (userDetails)
+          setUserDetails(userDetails)
+      } catch (e) {
+        //this.setState({ isError: true })
+      }
+    }
+    getUserDetails()
+  }, []);
+
+  return (
+    <div className={'header'}>
+      <div className={'header-wrapper'}>
+        <span className={'header-title'}>Glow Tox</span>
+        <div className={'profile-name'}>
+          <div className={'profile-inner-wrapper'} onClick={() => history.push({ pathname: "/profile" })}>
+            <div className={'profile-icon'}>
+              {
+                <img
                   className={'profile-icon-img'}
-                  src={'https://pbs.twimg.com/profile_images/1227540838596562944/bs5ZQkrs.jpg'} />
-                }
-              </div>
-              <div className={'profile-text'}> {'Suneet Patil'}</div>
+                  src={userDetails.profilePic || ''} />
+              }
             </div>
+            <div className={'profile-text'}> {userDetails.userName || ''}</div>
           </div>
         </div>
-        {/* <Divider /> */}
       </div>
-    );
-  
+    </div>
+  );
 }
 export default Header
