d58dd8411cb36e9d0b7159b0ea13aa489fe771af
akash hamse <akashhamse@akashs-MacBook-Air.local>
2021-02-06 12:23:27 +0530

    functional component dashboard

--- src/routes/dashboard/index.js
+++ src/routes/dashboard/index.js
@@ -1,55 +1,55 @@
-import React from 'react';
-import { withRouter, } from "react-router-dom";
+//import React from 'react';
+//import { withRouter, } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 import { PageTitle } from '../../components/page-title/'
 import { DASHBOARD_OPTIONS } from '../../constants/';
 import './dashboard.scss';
 import { Card } from 'antd';
 const { Meta } = Card;
 
-class Dashboard extends React.Component {
-
-  itemClicked = (index) => {
-    switch (index) {
-      case 0: this.props.history.push('/services')
-        break;
-      case 1: this.props.history.push('/promotions')
-        break;
-      case 2: this.props.history.push('/servicehistory')
-        break;
-      case 3: this.props.history.push('/giftcards')
-        break;
-      default: break;
-    }
-  }
-
-  render() {
-    return (
-      <div className="dashboard-screen">
-        <div className={'content-wrapper'}>
-          <PageTitle
-            title={'Dashboard'}
-          />
-          <div
-            className={'content-body-wrapper'}>
-            {
-              DASHBOARD_OPTIONS.map((option, index) =>
-                <div className={'dashboard-card'}
-                  onClick={() => this.itemClicked(index)} >
-                  <Card
-                    hoverable style={option.color}
-                  >
-                    {option.img}
-                    <Meta
-                      title={option.title}
-                      description={option.desc} />
-                  </Card>
-                </div>
-              )
-            }
-          </div>
-        </div>
-      </div>
-    );
+const ItemClicked = (index, history) => {
+  switch (index) {
+    case 0: history.push({ pathname:  "/services" });
+      break;
+    case 1: history.push({ pathname:  "/promotions" });
+      break;
+    case 2: history.push({ pathname:  "/servicehistory" });
+      break;
+    case 3: history.push({ pathname:  "/giftcards" });
+      break;
+    default: break;
   }
 }
-export default withRouter(Dashboard)
+
+const Dashboard = () => {
+  const history = useHistory();
+  return (
+    <div className="dashboard-screen">
+      <div className={'content-wrapper'}>
+        <PageTitle
+          title={'Dashboard'}
+        />
+        <div
+          className={'content-body-wrapper'}>
+          {
+            DASHBOARD_OPTIONS.map((option, index) =>
+              <div 
+                key={index}
+                className={'dashboard-card'}
+                onClick={() => ItemClicked(index, history)} >
+                <Card
+                  hoverable style={option.color}>
+                  {option.img}
+                  <Meta
+                    title={option.title}
+                    description={option.desc} />
+                </Card>
+              </div>
+            )
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Dashboard
